adb613c2c825732cb59076dc343e5534084fad2e
Niklas van Schrick <mc.taucher2003@gmail.com>
2026-03-06 19:20:14 +0100

    Improve release create_canary command

--- lib/pyxis/commands/release.rb
+++ lib/pyxis/commands/release.rb
@@ -3,12 +3,13 @@
 module Pyxis
   module Commands
     class Release < Thor
+      include SemanticLogger::Loggable
       include PermissionHelper
 
       desc 'create_canary', 'Promote an experimental build to canary'
       exclusive do
         at_least_one do
-          method_option :build,
+          method_option :build_id,
                         aliases: '-b',
                         desc: 'The build ID',
                         required: false,
@@ -23,23 +24,28 @@ module Pyxis
       def create_canary
         assert_executed_by_delivery_team_member!
 
-        build_id = options[:build] || ManagedVersioning::ComponentInfo.new(
+        info = ManagedVersioning::ComponentInfo.new(
+          build_id: options[:build_id],
           container_tag: options[:container_tag]
-        ).find_build_id_for_container_tag
+        )
 
-        raise Pyxis::MessageError, 'This build does not exist' if build_id.nil?
+        raise Pyxis::MessageError, 'This build does not exist' if info.find_build_id_for_container_tag.nil?
+        raise Pyxis::MessageError, 'This build does not exist' if info.find_container_tag_for_build_id.nil?
 
         pipeline = GitlabClient.client.create_pipeline(
           Project::Pyxis.api_gitlab_path,
           Project::Pyxis.default_branch,
           variables: {
-            PIPELINE_NAME: "Release build #{build_id} as canary",
+            PIPELINE_NAME: "Release #{info.find_container_tag_for_build_id} as canary",
             RELEASE_COORDINATOR: 'canary',
-            BUILD_ID_TO_PROMOTE: build_id.to_s,
+            BUILD_ID_TO_PROMOTE: info.find_build_id_for_container_tag.to_s,
           }
         )
 
-        raise Pyxis::MessageError, 'Failed to create pipeline' if pipeline.response.status != 201
+        if pipeline.response.status != 201
+          logger.warn('Failed to create pipeline for canary release', response: response.body)
+          raise Pyxis::MessageError, 'Failed to create pipeline'
+        end
 
         "Created coordinator pipeline at #{pipeline.body.web_url}"
       end
